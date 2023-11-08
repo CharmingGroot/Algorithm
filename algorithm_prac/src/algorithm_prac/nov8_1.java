d4917ff0e950e1d62d41aff2ba98c3866d3d0d5d
CharmingGroot <ohyes9711@gmail.com>
2023-11-08 22:06:29 +0900

    1108

---
+++ algorithm_prac/src/algorithm_prac/nov8_1.java
@@ -0,0 +1,10 @@
+package algorithm_prac;
+import java.util.*;
+
+class nov8_1 {
+    public String solution(String my_string, int n) {
+        String answer = my_string.substring(0,n);
+
+        return answer;
+    }
+}
