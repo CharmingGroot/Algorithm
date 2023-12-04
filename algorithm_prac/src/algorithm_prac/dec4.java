1c691312eb5280e0e277825d26dceb9a1409056a
CharmingGroot <ohyes9711@gmail.com>
2023-12-04 22:19:43 +0900

    1204

---
+++ algorithm_prac/src/algorithm_prac/dec4.java
@@ -0,0 +1,9 @@
+package algorithm_prac;
+import java.util.*;
+class dec4 {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+       
+        return answer;
+    }
+}
