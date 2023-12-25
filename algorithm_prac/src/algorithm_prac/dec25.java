ee8ba5a10354b14ad8363dbf63fc4c42a28615a2
CharmingGroot <ohyes9711@gmail.com>
2023-12-25 23:13:02 +0900

    1225

---
+++ algorithm_prac/src/algorithm_prac/dec25.java
@@ -0,0 +1,10 @@
+package algorithm_prac;
+import java.lang.Math;
+
+class dec25 {
+    public int solution(double flo) {
+        int answer = (int) Math.floor(flo);
+        
+        return answer;
+    }
+}
