f4e1ba480c711b3d6d9898d1c30514fe95223802
CharmingGroot <ohyes9711@gmail.com>
2023-12-27 20:55:50 +0900

    1227

---
+++ algorithm_prac/src/algorithm_prac/dec27_1.java
@@ -0,0 +1,9 @@
+package algorithm_prac;
+class dec27_1 {
+    public String solution(String n_str) {
+        int i = Integer.parseInt(n_str);
+        String answer = i+"";
+        
+        return answer;
+    }
+}
