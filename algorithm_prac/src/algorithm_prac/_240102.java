9875785123f68f51f9953bf02a9806c4374ffd09
CharmingGroot <ohyes9711@gmail.com>
2024-01-02 21:16:57 +0900

    240102

---
+++ algorithm_prac/src/algorithm_prac/_240102.java
@@ -0,0 +1,13 @@
+package algorithm_prac;
+class _240102 {
+
+    public int solution(String my_string, String target) {
+        int answer = 0;
+        
+        if(my_string.contains(target)){
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
