8d7f947ccbffd418b5c8c2d8ee168ddd210bb6b1
CharmingGroot <ohyes9711@gmail.com>
2024-01-09 21:33:30 +0900

    240109

---
+++ algorithm_prac/src/algorithm_prac/_240109.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class _240109 {
+    public String solution(String myString) {
+        String answer = "";
+        
+        char[] char_arr = myString.toCharArray();
+                
+        for(char ch : char_arr){
+            if(ch < 108) answer += "l";
+            else answer += ch;
+        }
+        
+        return answer;
+    }
+}
