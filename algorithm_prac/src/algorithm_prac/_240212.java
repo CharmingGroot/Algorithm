1efdf7541cfa53b9ad2ecc65dd05aeaf50ea51d9
CharmingGroot <ohyes9711@gmail.com>
2024-02-12 23:04:50 +0900

    240212

---
+++ algorithm_prac/src/algorithm_prac/_240212.java
@@ -0,0 +1,14 @@
+package algorithm_prac;
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        
+        String[] list = String.valueOf(n).split("");
+        Arrays.sort(list);
+ 
+        StringBuilder sb = new StringBuilder();
+        for (String aList : list) sb.append(aList);
+ 
+        return Long.parseLong(sb.reverse().toString());    
+    }
+}
